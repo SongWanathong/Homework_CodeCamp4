7d5c91bfa76ace8d3b47c12078a43f97c1d14074
song23284 <58030142@kmitl.ac.th>
2020-03-13 23:29:00 +0700

    create react homework

---
+++ week4/Basic React Day 1/my-app/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const app = (
+    <div>
+        
+        <h1  >
+           <spanf classname="fff">L</spanf>.
+           <span>O</span>.
+           <spanf>V</spanf>.
+           <span>E</span>
+
+
+        </h1>
+        
+    </div>
+)
+
+
+ReactDOM.render(app, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('gogo'));

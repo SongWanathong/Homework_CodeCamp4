04b6aadd9d0325f585023cac5ea7323f74785cd1
song23284 <58030142@kmitl.ac.th>
2020-03-14 21:02:57 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	modified:   src/index.js

--- week4/Basic React Day 1/my-app/src/App.js
+++ week4/Basic React Day 1/my-app/src/App.js
@@ -5,17 +5,17 @@ class Popup extends React.Component{
 render() {
   return (
     <div className='container'>
-                <div class="card text-center">
-                    <div class="card-header">
-                        <h5 class="card-title">{this.props.heading}</h5>
+                <div className="card ">
+                    <div className="card-header">
+                        <h5 className="card-title">{this.props.heading}</h5>
 
                     </div>
-                    <div class="card-body">
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <div className="card-body">
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
 
                     </div>
-                    <div class="card-footer text-muted">
-                        <a href="#" class="btn btn-light">Go somewhere</a>
+                    <div className="card-footer text-muted">
+                        <a href="#" className="btn btn-light">Go somewhere</a>
                     </div>
                 </div>
             </div>
--- week4/Basic React Day 1/my-app/src/index.js
+++ week4/Basic React Day 1/my-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Popup from './App';
 import Todo from './todo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
